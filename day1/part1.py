af95f411133fefe018a7cd4de5b2378ceaa929eb
MatthewVologzhin <matvey.vologzhin@mail.ru>
2022-02-16 11:22:55 +0700

    Update 16.02.2022

---
+++ day1/part1.py
@@ -0,0 +1,80 @@
+import time as t
+import sys
+class SomeException(BaseException):
+    None
+
+class FinalException(BaseException):
+    def __init__(self, j = None):
+        with open('output1.txt', 'w') as O:
+            O.write(str(j))
+        print('Ура! Мы смогли вычислить, что Санта дойдёт до {} этажа.'.format(j))
+        t.sleep(5)
+        while True:
+            print('Попробуете ещё раз?')
+            Vote = input('(Да/Нет): ')
+            print()
+            if Vote.lower() == 'да':
+                print('\n')
+                break
+            elif Vote.lower() == 'нет':
+                print('\n')
+                print('Спасибо за помощь Санте! До встречи в следующем году! Хоу-хоу-хоу...')
+                t.sleep(5)
+                sys.exit(1)       
+            else:
+                print('*Санта пытается понять, что Вы сказали, но лишь вопросительно смотрит на Вас.* \n')   
+class EndException(BaseException):
+    None
+    
+
+
+print('Хоу-хоу-хоу, уже готов помочь Санте? Что ж, приступим!\n')
+t.sleep(2)
+
+while True:
+    try:
+        FloorUp = 0
+        FloorDown = 0
+
+        text = input('Введите ТАЙНОЕ сообщение для Санты (или "1" для проверки задания): ')
+        if text == '1':
+            with open('input.txt', 'r') as I:
+                FloorUp = 0
+                FloorDown = 0
+                k = 0
+                for i in I:
+                    for j in range(len(i)):
+                        if i[j] == '(':
+                            FloorUp += 1
+                        elif i[j] == ')':
+                            FloorDown +=1
+                        else:
+                            print('*Санта качает головой, не понимая, что ты имеешь в виду*\n')
+                            raise SomeException()
+        else:
+            FloorUp = text.count('(')
+            FloorDown = text.count(')')
+            print(FloorUp)
+            print(FloorDown)
+            print(len(text))
+            if len(text) == (FloorUp + FloorDown):
+                raise FinalException(FloorUp - FloorDown)
+            else:
+                print('*Санта качает головой, не понимая, что ты имеешь в виду*\n')
+                raise SomeException()
+        Expression = FloorUp - FloorDown
+        raise FinalException(Expression)
+    
+    except SomeException:
+        continue
+    except FinalException:
+        None          
+    except EndException:
+        break
+
+print('Спасибо за помощь Санте! До встречи в следующем году! Хоу-хоу-хоу...')
+t.sleep(5)
+sys.exit(1)
+        
+        
+    
---
+++ day1/part2.py
@@ -0,0 +1,87 @@
+import time as t
+import sys
+class SomeException(BaseException):
+    None
+
+class FinalException(BaseException):
+    def __init__(self, j = None):
+        with open('output2.txt', 'w') as O:
+            O.write(str(j))
+        print('Ура! Мы смогли вычислить, что Санта дойдёт до подвала на {} шаге.'.format(j))
+        t.sleep(5)
+class EndException(BaseException):
+    None
+    
+
+
+print('Хоу-хоу-хоу, теперь нам предстоит узнать, на каком шаге Санта попадёт ' +
+      '\nв подвал! Кто знает, для чего ему это... \nНаверное, там' +
+      'что-то действительно важное или интересное.\n')
+
+while True:
+    try:
+        FloorUp = 0
+        FloorDown = 0
+
+        text = input('Введите ТАЙНОЕ сообщение для Санты (или "0" для проверки задания): ')
+        if text == 0:
+            with open('input.txt', 'r') as I:
+                FloorUp = 0
+                FloorDown = 0
+                k = 0
+                for i in I:
+                    for j in range(len(i)):
+                        k += 1
+                        if i[j] == '(':
+                            FloorUp += 1
+                        elif i[j] == ')':
+                            FloorDown +=1
+                            if (FloorUp - FloorDown) == -1:
+                                raise FinalException(k)
+                        else:
+                            raise ZeroDivisionError
+
+        j = 0
+        for i in text:
+            j += 1
+            if i == '(':
+                FloorUp += 1
+            elif i == ')':
+                FloorDown += 1
+                if FloorUp - FloorDown == -1:
+                    raise FinalException(j)
+            else:
+                raise ZeroDivisionError
+
+        print('К сожалению, Санте не удастся побывать на -1 этаже в этот раз :c')
+        while True:
+            print('Попробуете ещё раз?')
+            Vote = input()
+            if Vote.lower() == 'да':
+                print('\n')
+                break
+            elif Vote.lower() == 'нет':
+                print('\n')
+                raise EndException()
+            else:
+                print('*Санта пытается понять, что Вы сказали, но лишь вопросительно смотрит на Вас.* \n')
+
+        
+        
+            
+            
+    except ZeroDivisionError:
+        print('Решили по-другому шифровать? Увы, Санта ждёт от Вас другой шифр! \n')
+    except SomeException:
+        continue
+    except FinalException:
+        break            
+    except EndException:
+        break
+
+print('Спасибо за помощь Санте! До встречи в следующем году! Хоу-хоу-хоу...')
+t.sleep(5)
+sys.exit(1)
+        
+        
+    
