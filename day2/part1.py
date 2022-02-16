919eb7ac653b81e23b86fddc9cbfe9edb2cd6c7d
MatthewVologzhin <matvey.vologzhin@mail.ru>
2022-02-16 14:32:15 +0700

    Update 16.02.2022

---
+++ day2/part1.py
@@ -0,0 +1,23 @@
+def func(I):
+    global area
+    string = line
+    size = string.split('x')
+    l = int(size[0])
+    w = int(size[1])
+    h = int(size[2])
+
+    area_list = [l*w, w*h, h*l]
+    min_area = min(area_list)
+
+    area += 2*(area_list[0]+area_list[1]+area_list[2]) + min_area
+    return area
+  
+with open('input.txt', 'r') as I:
+    area = 0
+    for line in I:
+        func(line)
+
+print('Площадь обёрточной бумаги, которую нужно заказать: '+ str(area) + ' футов^2')
+
+
+    
