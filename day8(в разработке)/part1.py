988ae334d003b6e5c77088d8d1262559d4dd3385
MatthewVologzhin <matvey.vologzhin@mail.ru>
2022-02-28 13:31:41 +0700

    Update 28.02.2022

--- day8(в разработке)/part1.py
+++ day8(в разработке)/part1.py
@@ -3,25 +3,29 @@ import itertools as perm
 string = 'abcdefghijklmnopqrstuvwxyz0123456789'
 array = list(perm.product(string, repeat=2))
 for i in range(len(array)):
-    array[i] = '\\x' + array[i][0] + array[i][1] + '\\'
+    array[i] = '\\x' + array[i][0] + array[i][1]
 
-real_chars = 0
-comp_chars = 0
+r_string = ''
+c_string = ''
+
+print('Помогаем Санте определить количество неучитываемых символов...\n')
+
 with open('input.txt', 'r') as INPUT:
     for line in INPUT:
-        real_chars += len(line)
         line = line.strip()
+        r_string += line
         line = line[1:-1]
-        for element in array:
-            if element in line:
-                line = line.replace(element, '*')
-        if '\\"' in line:
-            line = line.replace('\\"','\"')
         if '\\\\' in line:
-            line = line.replace('\\\\','\\')
-        comp_chars += len(line)
+            line = line.replace('\\\\','*')
+        if '\\"' in line:
+            line = line.replace('\\"','*')
+        for element in array:
+            if element in array:
+                line = line.replace(element, '*')
+        c_string += line
+    value = len(r_string) - len(c_string)
+    print('У Санты не будет учтено {} символа!'.format(value))
 
-        print(line)
-        print(len(line))
-print(real_chars - comp_chars)
-            
+with open('output1.txt','w') as OUTPUT:
+    OUTPUT.write(str(value))
+    
--- day8(в разработке)/part2.py
+++ day8(в разработке)/part2.py
@@ -1,76 +1,28 @@
-print('Бобу нужна помощь с проводами и расчётами... Давайте поможем!\n')
+import itertools as perm
 
-array = []
+string = 'abcdefghijklmnopqrstuvwxyz0123456789'
+array = list(perm.product(string, repeat=2))
+for i in range(len(array)):
+    array[i] = '\\x' + array[i][0] + array[i][1]
 
-#Функция прохода по проводам
-def score(start_element, array):
-    letter_dict = {x[1]: x[0] for x in start_element}
-    for i in range(len(array)):
-        for element in array:
- 
-            if element[1] == 'RSHIFT' and element[0] in letter_dict.keys():
-                values = int(letter_dict.get(element[0])) >> int(element[2])
-                letter_dict.update({element[-1]: values})
+old_str = ''
+new_str = ''
 
-            elif element[1] == 'LSHIFT' and element[0] in letter_dict.keys():
-                values = int(letter_dict.get(element[0])) << int(element[2])
-                letter_dict.update({element[-1]: values})
+print('Помогаем Санте определить количество неучитываемых символов...\n')
 
-            elif element[1] == 'NOT' and element[0] in letter_dict.keys():
-                values = ~int(letter_dict.get(element[0]))
-                letter_dict.update({element[-1]: values})
-
-            elif element[1] == 'OR':
-                if element[0] in letter_dict.keys() and element[2] in letter_dict.keys():
-                    values = int(letter_dict.get(element[0])) | int(letter_dict.get(element[2]))
-                    letter_dict.update({element[-1]: values})
-                elif element[0].isdigit() and element[2] in letter_dict.keys():
-                    values = int(element[0]) | int(letter_dict.get(element[2]))
-                    letter_dict.update({element[-1]: values})
-                elif element[0] in letter_dict.keys() and element[2].isdigit():
-                    values = int(letter_dict.get(element[0])) | int(element[2])
-                    letter_dict.update({element[-1]: values})
-
-            elif element[1] == 'AND':
-                if element[0] in letter_dict.keys() and element[2] in letter_dict.keys():
-                    values = int(letter_dict.get(element[0])) & int(letter_dict.get(element[2]))
-                    letter_dict.update({element[-1]: values})
-                elif element[0].isdigit() and element[2] in letter_dict.keys():
-                    values = int(element[0]) & int(letter_dict.get(element[2]))
-                    letter_dict.update({element[-1]: values})
-                elif element[0] in letter_dict.keys() and element[2].isdigit():
-                    values = int(letter_dict.get(element[0])) & int(element[2])
-                    letter_dict.update({element[-1]: values})
-            elif len(element) == 2 and element[0].isalpha() and element[0] in letter_dict.keys():
-                values = letter_dict.get(element[0])
-                letter_dict.update({element[1]: values})
-    return letter_dict.get('a')
-
-# Преобразуем строки в нужный вид
 with open('input.txt', 'r') as INPUT:
     for line in INPUT:
         line = line.strip()
-        line = line.split()
-        line.remove('->')
-        array.append(line)
+        old_str += line
+        if '\"' in line:
+            line = line.replace('\"', '**')
+        if '\\' in line:
+            line = line.replace('\\', '**')
+        line = '\"' + line + '\"'
+        new_str += line
+    value = len(new_str) - len(old_str)
+    print('Разница между новой и старой строкой будет {} символов!'.format(value))
 
-# Ищем начальный элемент и смещаем "NOT" на одну позицию справа
-start_element = []
-for element in array:
-    if element[0].isdigit() and len(element) == 2:
-        start_element.append(element)
-        
-    if element[0] == 'NOT':
-        element[0] = element[1]
-        element[1] = 'NOT'
-
-# Первый расчёт
-a = score(start_element, array)
-
-# Второй расчёт
-start_element[0][0] = str(a)
-a = score(start_element, array)
-
-with open('output2.txt', 'w') as OUTPUT:
-    OUTPUT.write(str(a))
-print('В итоге Боб получит число: ', str(a))
+with open('output2.txt','w') as OUTPUT:
+    OUTPUT.write(str(value))
+    
