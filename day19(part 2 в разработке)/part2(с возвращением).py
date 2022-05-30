ae554e69d3009f612947d859a244299f5fa07360
MatthewVologzhin <matvey.vologzhin@mail.ru>
2022-05-30 15:39:20 +0700

    Update 30.05.2022

--- day19(part 2 в разработке)/part2(с возвращением).py
+++ day19(part 2 в разработке)/part2(с возвращением).py
@@ -1,5 +1,8 @@
 print('Изучаем разные варианты построения молекулы...')
 
+from random import shuffle,randint
+from re import split as spl
+
 with open('input.txt','r') as INPUT:
     array = []
     for line in INPUT:
@@ -12,14 +15,41 @@ with open('input.txt','r') as INPUT:
             main_moleculs = line
 array.sort(key=lambda x: len(x[1]), reverse = True)
 
-def minimize(moleculs, array):
-    for trans in array:
-        if trans[1] in moleculs:
-            pos = moleculs.find(trans[1])
-            moleculs = moleculs[:pos] + trans[0] + moleculs[pos + len(trans[1]):]
-    if moleculs == 'e':
-        return moleculs
-    else:
-        raise ZeroDivisionError
+def minimize(main, array):
+    try:
+        while True:
+            moleculs = main
+            counter = 0
+            switcher = 0
+            moleculs1 = None
+            moleculs2 = 0
+            while moleculs1 != moleculs2:
+                counter += 1
+                shuffle(array)
+                for trans in array:
+                    if trans[1] in moleculs:
+                        moleculs = spl(trans[1], moleculs)
+                        print(moleculs)
+                        if len(moleculs) == 1:
+                            continue
+                        A = randint(1,len(moleculs)-1)
+                        for i in range(1,2*len(moleculs)-1,2):
+                            moleculs.insert(i,trans[1])
+                        moleculs[2*A-1] = trans[0]
+                        moleculs = ''.join(moleculs)    
+                        if moleculs == 'e':
+                            print(1)
+                            raise ZeroDivisionError
+                        break
+                if switcher == 1:
+                    break
+                if moleculs1 == None:
+                    moleculs1 = moleculs
+                else:
+                    moleculs2 = moleculs
+                    switcher = 1
+    except ZeroDivisionError:
+        print(counter)
+        print('Nice')
 minimize(main_moleculs, array)
 
